18e32bd173872806e39485b0c2a2a3790697b9e4
Miguel Motta <93295363+MIGUEL-MOTTA-U@users.noreply.github.com>
2024-12-23 17:49:26 -0500

    fix: arreglando verificación de commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,19 @@
 export default {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf']
+      ],
+      'type-case': [2, 'always', 'lowerCase'],
+      'type-empty': [2, 'never'],
+      'subject-empty': [2, 'never'],
+      'subject-full-stop': [2, 'never', '.'],
+      'subject-case': [
+        2,
+        'always',
+        ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']
+      ]
     }
+  }
